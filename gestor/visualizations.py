f7d9300d8640f6836ccd9465d4766180ca08d6bc
Castipy <claudiaarrietacastellanos@gmail.com>
2025-06-22 22:50:56 -0700

    Modificaciones a graficos de barras y se añadio el grafico de lineplot, se unificaron todos los metodos de graficar

--- gestor/visualizations.py
+++ gestor/visualizations.py
@@ -14,87 +14,48 @@ class Graphs:
     Atributos:
         manager (FinanceManager): Instancia de la clase FinanceManager que contiene las transacciones.
     Métodos:
-        categories_expenses_graphic: Genera un gráfico de pastel de gastos por categoría.
-        monthly_expenses_graphic: Genera un gráfico de barras de gastos diarios para un mes y año específicos.
-        anual_expenses_graphic: Genera un gráfico de barras de gastos anuales.
+        expenses_graphics: Genera gráficos de gastos en diferentes formatos (barra, pastel, línea).
         save_graph: Guarda el gráfico generado en un archivo.
-        plot_graph: Método auxiliar para crear y mostrar el gráfico.
     '''
     DATE_FORMAT = '%d-%m-%Y %H:%M:%S'
-    
+
     def __init__(self, manager:FinanceManager):
         self.manager = manager
         
-
-    def categories_expenses_graphic(self) -> None:
-        '''Genera un gráfico de pastel de gastos por categoría.'''
-
-        expenses = self.manager.categories_expenses()
-        df = pd.DataFrame(list(expenses.items()), columns=['Category', 'Amount'])
-        self.plot_graph('pie', df['Amount'], labels=df['Category'], title='Gastos por Categoría')
-
-    def monthly_expenses_graphic(self, year: str | None = None, month: str | None = None) -> None:
-        '''Genera un gráfico de barras de gastos diarios para un mes y año específicos.'''
-        try:
-            year = int(year) if year else pd.Timestamp.now().year
-            month = int(month) if month else pd.Timestamp.now().month
-        except ValueError:
-            print("Año y mes deben ser números enteros.")
-            return
-
-        expenses = self.manager.historial_expenses()
-        df = pd.DataFrame([{'Date': t.date, 'Amount': t.amount} for t in expenses])
-        df['Date'] = pd.to_datetime(df['Date'], format=self.DATE_FORMAT)
-        df.set_index('Date', inplace=True)
-
-        df_monthly_expenses = df[(df.index.year == year) & (df.index.month == month)]
-        if df_monthly_expenses.empty:
-            print(f"\nNo hay gastos registrados para el mes {month} del año {year}.")
-            return
-
-        df_daily_expenses = df_monthly_expenses.resample('D').sum()
-        self.plot_graph('bar', df_daily_expenses, title=f'Gastos diarios - {month:02d}-{year}', format='%d')
-
-    def anual_expenses_graphic(self) -> None:
-        '''Genera un gráfico de barras de gastos anuales.'''
-        expenses = self.manager.historial_expenses()
-        if not expenses:
-            print("No hay gastos registrados para mostrar en el gráfico.")
-            return
-        df = pd.DataFrame({'Date': t.date, 'Amount': t.amount} for t in expenses)
-        df['Date'] = pd.to_datetime(df['Date'], format=self.DATE_FORMAT)
-        df.set_index('Date', inplace=True)
-        df_anual_expenses = df.resample('YE').sum()
-        self.plot_graph('bar', df_anual_expenses, title='Gastos Anuales', format='%Y')
-
-   
-    def save_graph(self, parent_file:str, filename:str) -> None:
-        '''Guarda el gráfico generado en un archivo.'''
-        os.makedirs(parent_file, exist_ok=True)
-        filename = os.path.basename(filename)
-        ruta_imagen = os.path.join(parent_file, filename)
-        plt.savefig(ruta_imagen)
-        print(f"Gráfica guardada como: {ruta_imagen}")
-
-    
-    def plot_graph(self, graph_type:str, values:pd.DataFrame, title:str, labels:List[str]=None, format=None) -> plt.Figure:
-        '''Crea, muestra y devuelve el objeto matplotlib Figure generado.'''
-        
-        if graph_type not in {'pie', 'bar'}:
-            raise ValueError(f"Tipo de gráfico no soportado: {graph_type}")
-        
+    def expenses_graphics(self, df:pd.DataFrame, title:str, graph_type:str = 'bar')-> plt.Figure:
+        '''Genera gráficos de gastos en diferentes formatos (barra, pastel, línea).'''
+        #Tamaño de la figura#
         plt.figure(figsize=(6, 4))
+        #Tema y paleta de colores#
         sns.set_theme(style="whitegrid")
         sns.set_palette("pastel")
-    
+        #Filtrando por el tipo de grafico#
         if graph_type == 'pie':
-            plt.pie(values, labels=labels, autopct='%1.1f%%', startangle=140)
+            plt.pie(df['Amount'], labels=df['Category'],autopct=lambda p: '{:.0f}'.format(p * df['Amount'].sum() / 100), startangle=140)
             plt.title(title)
-        elif graph_type == 'bar':
-            sns.set_context("talk")
-            values = values.copy()
-            values['DateStr'] = values.index.strftime(format)
-            sns.barplot(data=values, x='DateStr', y='Amount', color='skyblue')
+        elif graph_type == 'lineplot':
+            #Suma los valores de todas las columnas por filas#
+            df['Total'] = df.iloc[:, 1:].sum(axis=1)
+            #Asignando los días (columns[0]='Date') como el índice del DF#
+            df = df.set_index(df.columns[0])
+            #Reindexando para obtener todos los dias y los vacios se rellenan con 0#
+            df = df.reindex(range(1,32), fill_value=0)
+            #Se resetea el indice para que se convierta a columna#
+            df.reset_index(inplace=True)
+            #Renombrando la columna que era el indice(index el cual reseteamos) a Date#
+            df.rename(columns={df.columns[0]:'Date'}, inplace=True)
+            sns.lineplot(data=df, x='Date', y='Total')
+            plt.xticks(range(1,32,1))
+            plt.xlabel("Día")
+            plt.ylabel("Monto ($)")
+            plt.title(title)
+        else:
+            df = df.reset_index()
+            sns.barplot(data=df, x=df.columns[1], y='Amount', color='skyblue')
+            #Obtenemos el valor maximo del eje Y#
+            max_y = plt.gca().get_ylim()[1]
+            #Forzamos a que los valores en Y tengan una distancia de 200u#
+            plt.yticks(range(0, int(max_y) + 100, 200))
             plt.xticks(rotation=0)
             plt.title(title)
             plt.xlabel("Fecha")
@@ -102,8 +63,16 @@ class Graphs:
 
         fig = plt.gcf()
         safe_title = title.replace('/', '-')
-        self.save_graph("graficas", safe_title + '.png')
+        self.save_graph(safe_title + '.png') 
         plt.tight_layout()
         plt.show()
         plt.close(fig)  # Libera memoria si se generan muchas figuras
-        return fig
+        return fig    
+
+    def save_graph(self, filename:str) -> None:
+        '''Guarda el gráfico generado en un archivo.'''
+        os.makedirs('images', exist_ok=True)
+        filename = os.path.basename(filename)
+        ruta_imagen = os.path.join('images', filename)
+        plt.savefig(ruta_imagen)
+        print(f"Gráfica guardada como: {ruta_imagen}")
