047f27c3f38955c63c1e17f93bee38d9ff0f0bbe
Castipy <claudiaarrietacastellanos@gmail.com>
2025-06-22 22:47:57 -0700

    Nuevas opciones para mostrar graficas y gastos en tablas

--- gestor/main.py
+++ gestor/main.py
@@ -1,22 +1,22 @@
 from transactions import Transactions
 from core import FinanceManager
 from visualizations import Graphs
-import os
+import pandas as pd
 
-##Cargando Archivo CSV##
+#Iniciando instancias#
 manager= FinanceManager()
 graphs = Graphs(manager)
-manager.load_csv(filename=os.path.join(os.path.dirname(os.path.dirname(__file__)),'data/data.csv'))
+##Cargando Archivo CSV##
+manager.load_excel()
 
 def menu():
     print("\n=== Gestor de Gastos ===")
     print("1. Agregar ingreso")
     print("2. Agregar gasto")
     print("3. Ver balance total")
-    print("4. Ver todos los gastos")
-    print("5. Ver gastos por categoría")
-    print("6. Gráficas")
-    print("7. Guardar y salir")
+    print("4. Ver Gastos")
+    print("5. Gráficas")
+    print("6. Guardar y salir")
 
 while True:
     menu()
@@ -51,52 +51,142 @@ while True:
     elif choice == '3': ##Ver balance total##
         total = manager.total_balance()
         print(f"\nEl balance total es: {total}")
-    
-    elif choice == '4': ##Ver todos los gastos##
-        expenses = manager.historial_expenses()
-        if expenses:
-            print("\nHistorial de gastos:")
-            for expense in expenses:
-                print(expense)
-        else:
-            print("\nNo hay gastos registrados.")
 
-    elif choice == '5': ##Ver gastos por categoría##
-        expenses_by_category = manager.categories_expenses()
-        if expenses_by_category:
-            print("\nGastos por categoría:")
-            for category, total in expenses_by_category.items():
-                print(f"{category}: {total}")
-        else:
-            print("\nNo hay gastos registrados por categoría.")
+    elif choice == '4': ##Ver gastos##
+        _,resumen = manager.expenses()
+        if resumen.empty:
+                print("\nNo hay gastos registrados")
+                continue
+        print('\nQue tipo de datos desea ver?',
+              '\n1. Gastos Totales',
+              '\n2. Gastos Mensuales',
+              "\n3. Gastos Diarios"
+              "\n4. Gasto Anual"
+              "\n5. Gastos Anuales (últimos 10 años)")
+        selection = input("Seleccione una opción: ")
 
-    elif choice == '6': ##Ver gráfico de gastos por categoría##
-        expenses = manager.historial_expenses()
-        if not expenses:
-            print("\nNo hay gastos registrados para graficar.")
-            continue
-        selection = input("\nTipos de gráficos disponibles:"
-                         "\n1. Gastos por Categorías"
-                         "\n2. Gastos Diarios por mes"
-                         "\n3. Gastos Mensuales por año"
-                         "\nSeleccione una opción: ")
         if selection == '1':
-            graphs.categories_expenses_graphic()
+            resumen = resumen.set_index('Category').T
+            print(resumen)
+            total= resumen.iloc[0].sum()
+            print(f'\nTotal de gastos {total}')
         elif selection == '2':
-            print("\nPara obtener datos actuales dejar vacío el año y mes.")
+            print('\nPara obtener estadisticas actuales NO introduzca datos')
             year = input("\nIngrese el año (YYYY): ")
             month = input("\nIngrese el mes (MM): ")
-            graphs.monthly_expenses_graphic(year, month)
+            resumen = manager.monthly_expenses(year, month)
+            if resumen.empty:
+                print(f"\n""No hay gastos registrados para el mes {month} del año {year}.")
+                continue
+            print(f'\nLos gastos del mes {month if month else pd.Timestamp.now().month} son:')
+            resumen = resumen.set_index('Category').T
+            print('\n', resumen)
+            total= resumen.iloc[0].sum()
+            print(f'\nTotal de gastos {total}')
         elif selection == '3':
-            graphs.anual_expenses_graphic()
+            print('\nPara obtener estadisticas actuales NO introduzca datos')
+            year = input("\nIngrese el año (YYYY): ")
+            month = input("\nIngrese el mes (MM): ")
+            resumen = manager.monthly_expenses(year, month, daily=True)
+            if resumen.empty:
+                print(f"\n""No hay gastos registrados para el mes {month} del año {year}.")
+                continue
+            print(f'\nLos gastos diarios para el mes {month if month else pd.Timestamp.now().month} son:')
+            print('\n', resumen.to_string(index=False))
+            total = resumen.iloc[:,1:].values.sum()
+            print(f'\nTotal de gastos {total}')
+        elif selection == '4':
+            print('\nPara obtener estadisticas actuales NO introduzca datos')
+            year = input("\nIngrese el año (YYYY): ")
+            resumen = manager.anual_expenses(year)
+            if resumen.empty:
+                print(f"\n""No hay gastos registrados para el año {year}.")
+                continue
+            resumen = resumen.reindex(range(1, 13), fill_value=0)
+            # Transponer para tener una sola fila y meses como columnas
+            resumen = resumen.T.to_frame().T
+            resumen.columns = [f'Mes_{m}' for m in resumen.columns]
+            resumen.index = [f'Año_{year}']
+            print(f'\nLos gastos del año {year if year else pd.Timestamp.now().year} son :')
+            print('\n', resumen.to_string(index=False))
+            total = resumen.values.sum()
+            print(f'\nTotal de gastos {total}')
+        elif selection == '5':
+            resumen = manager.anual_expenses(all_years=True)
+            if resumen.empty:
+                print(f"\n""No hay gastos registrados para los últimos 10 años.")
+                continue
+            actual_year = pd.Timestamp.now().year
+            resumen = resumen.reindex(range(actual_year-9,actual_year + 1),fill_value=0)
+            resumen = resumen.T.to_frame().T
+            print(f'\n Los gastos de los ultimos 10 años son:')
+            print('\n', resumen.to_string(index=False))
+            total= resumen.values.sum()
+            print(f'\nTotal de gastos {total}')
         else:
             print("\nOpción no válida. Por favor, seleccione una opción válida.")
             continue
 
-    elif choice == '7': ##Guardar y salir##
-        os.makedirs('data', exist_ok=True)
-        file_path= os.path.join('data',os.path.basename('data.csv'))
-        manager.save_csv(file_path)
+    elif choice == '5': ##Ver gráficos de gastos##
+        _,resumen = manager.expenses()
+        if resumen.empty:
+            print("\nNo hay gastos registrados")
+            continue
+        selection = input("\nTipos de gráficos disponibles:"
+                        "\n1. Gastos Mensuales"
+                        "\n2. Gastos Diarios"
+                        "\n3. Gastos Anuales"
+                        "\n4. Gastos de todos los Años"
+                        "\nSeleccione una opción: ")
+        if selection == '1':
+            print('\nPara obtener estadisticas actuales NO introduzca datos')
+            year = input("\nIngrese el año (YYYY): ")
+            month = input("\nIngrese el mes (MM): ")
+            resumen = manager.monthly_expenses(year, month)
+            if resumen.empty:
+                print(f"\n""No hay gastos registrados para la fecha.")
+                continue
+            title = (f'Gastos Mensuales {month if month else pd.Timestamp.now().month}-'
+                     f'{year if year else pd.Timestamp.now().year}')
+            graphs.expenses_graphics(resumen, title=title, graph_type='pie')
+
+        elif selection == '2':
+            print('\nPara obtener estadisticas actuales NO introduzca datos')
+            year = input("\nIngrese el año (YYYY): ")
+            month = input("\nIngrese el mes (MM): ")
+            resumen = manager.monthly_expenses(year, month, daily=True)
+            if resumen.empty:
+                print(f"\n""No hay gastos registrados para la fecha escogida.")
+                continue
+            title = (f'Gastos Diarios {month if month else pd.Timestamp.now().month}-'
+                     f'{year if year else pd.Timestamp.now().year}')
+            graphs.expenses_graphics(resumen, title=title, graph_type='lineplot')
+
+        elif selection == '3':
+            print('\nPara obtener estadisticas actuales NO introduzca datos')
+            year = input("\nIngrese el año (YYYY): ")
+            resumen = manager.anual_expenses(year)
+            if resumen.empty:
+                print("\n"f"No hay gastos registrados para el año {year if year else pd.Timestamp.now().year}.")
+                continue
+            resumen = resumen.reindex(range(1,13),fill_value=0).reset_index()
+            graphs.expenses_graphics(resumen, f'Gasto anual del {year if year else pd.Timestamp.now().year}')
+
+        elif selection == '4':
+            resumen = manager.anual_expenses(all_years=True)
+            print(resumen)
+            if resumen.empty:
+                print(f"\n""No hay gastos registrados para los últimos 10 años.")
+                continue
+            year = pd.Timestamp.now().year
+            resumen = resumen.reindex(range(year-9,year + 1),fill_value=0).reset_index()
+            graphs.expenses_graphics(resumen, f'Gastos de los últimos 10 años {year-10}-{year}' )
+        else:
+            print("\nOpción no válida. Por favor, seleccione una opción válida.")
+            continue
+
+    elif choice == '6': ##Guardar y salir##
+        file_path = manager.save_excel()
         print(f"\nDatos guardados en {file_path}. Saliendo...")
         break
 
